0a0df24adb52799abd69fd925ed05b49875e869e
Yash <yash.sharma2484@gmail.com>
2024-05-30 22:53:57 +0530

    Replace Hii with yashhshh

---
+++ demo.js
@@ -0,0 +1,9 @@
+const base_url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+for(let a of dropdowns){
+    console.log(a);
+}
+
+
+
